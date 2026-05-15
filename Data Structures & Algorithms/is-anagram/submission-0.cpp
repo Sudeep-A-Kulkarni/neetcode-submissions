95b49a237033a0ab2b4bd8412ce63af5f5862ae6
sudeep <kulkarnisudeep2005@gmail.com>
2026-05-15 23:31:14 +0530

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        bool bFlag = true;
+        int i = 0;
+        int arr[26] = {0};
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for ( i = 0; i < s.size(); i++) {
+            arr[s[i] - 'a'] ++;
+            arr[t[i] - 'a'] --;
+        }
+
+        for ( i = 0; i < 26; i++) {
+            if (arr[i] != 0) {
+                bFlag = false;
+            }
+        }
+        return bFlag;
+    }
+};
