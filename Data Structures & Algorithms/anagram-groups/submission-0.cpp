5fe7db657701eb8abc14fd9a7658bed011dfbd2f
sudeep <kulkarnisudeep2005@gmail.com>
2026-05-18 21:15:10 +0530

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> umap;
+
+        for (auto c : strs) {
+            string temp = c;
+            sort(c.begin(), c.end());
+            umap[c].push_back(temp);
+        }
+
+        for ( auto c : umap ) {
+            ans.push_back(c.second);
+        }
+       
+       return ans;
+    }
+};
