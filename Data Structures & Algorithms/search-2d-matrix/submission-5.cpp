608a84d11001c769af155a906b6b80f531cc06c2
sudeep <kulkarnisudeep2005@gmail.com>
2026-06-02 17:26:06 +0530

    Add: search-2d-matrix - submission-5

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int row = 0;
+        bool bFlag = false;
+
+        for (int i = 0; i < matrix.size(); i ++) {
+            if (!((matrix[i][matrix[0].size()-1]) < target)) {
+                row = i;
+                break;
+            }
+        }
+
+        for (int j = 0; j < matrix[0].size(); j++) {
+            if (matrix[row][j] == target) {
+                bFlag = true;
+                break;
+            }
+        }
+       return bFlag;
+    }
+};
